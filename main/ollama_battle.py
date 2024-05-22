942eb153f24b707da29167a8cd74d6bb6183d0b7
yulu86 <xuyulu86@126.com>
2024-05-22 22:31:32 +0800

    merge

--- main/ollama_battle.py
+++ main/ollama_battle.py
@@ -6,28 +6,17 @@ import requests
 import simpleaudio as sa
 import ollama
 
-<<<<<<< HEAD
-
-=======
 # 主题
->>>>>>> 43640737cc8c8e7dcf17aefc7199c7abf3069a31
 SUBJECT = ''
 
 # 是否使用OLLMA
 USE_OLLAMA = True
-<<<<<<< HEAD
-# OLLMA模型
-OLLMA_MODEL = 'qwen:32b-chat-v1.5-q4_0_stable_weibo'
-# safetensors模型
-SAFETENSORS_MODEL = 'Qwen1.5-32B-Chat-4Bit'
-=======
 
 # OLLMA模型
 OLLMA_MODEL1 = 'qwen:32b-chat-v1.5-q4_0_zhihu'
 USER1 = 'zhihu'
 OLLMA_MODEL2 = 'qwen:32b-chat-v1.5-q4_0_bilibot'
 USER2 = 'bilibili'
->>>>>>> 43640737cc8c8e7dcf17aefc7199c7abf3069a31
 
 def generate_speech(text, port):
     time_ckpt = time.time()
@@ -54,13 +43,8 @@ def split_text(text):
     text = re.sub(pattern, '', text)
     return text
 
-<<<<<<< HEAD
-def generate_by_ollama(question):
-    response = ollama.chat(model=OLLMA_MODEL, messages=[
-=======
 def generate_by_ollama(question, amodel):
     response = ollama.chat(model=amodel, messages=[
->>>>>>> 43640737cc8c8e7dcf17aefc7199c7abf3069a31
           {
             'role': 'user',
             'content': question,
@@ -70,22 +54,6 @@ def generate_by_ollama(question, amodel):
     )    
     return response['message']['content']
 
-<<<<<<< HEAD
-if __name__ == "__main__":    
-    
-
-    template = ''    
-
-    while True:        
-        print("%s: %s" % ("问题", question))
-        question = split_text(question)
-        generate_speech(question, 9880)        
-
-        time_ckpt = time.time()           
-        response = generate_by_ollama(question)    
-
-        print("%s: %s (Time %d ms)\n" % ("哔友", response, (time.time() - time_ckpt) * 1000))
-=======
 if __name__ == "__main__": 
     question = SUBJECT
 
@@ -108,6 +76,5 @@ if __name__ == "__main__":
         question = response
 
         print("%s: %s (Time %d ms)\n" % (USER1, response, (time.time() - time_ckpt) * 1000))
->>>>>>> 43640737cc8c8e7dcf17aefc7199c7abf3069a31
         response = split_text(response)
         generate_speech(response, 9881)
